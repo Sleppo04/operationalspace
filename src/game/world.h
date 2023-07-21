c1bfcb8d9338aa0689d39bb5d98a814aac1c9866
Hyalunar <vegotenksGogeto@gmx.de>
2023-07-21 14:05:06 +0200

    Additional properties and more docs

--- src/game/world.h
+++ src/game/world.h
@@ -1,6 +1,7 @@
 #ifndef WORLD_H
 #define WORLD_H
 
+#include <errno.h>
 #include <stdlib.h>
 
 #include "sector.h"
@@ -13,9 +14,13 @@ typedef struct World {
 
 typedef struct Feature {
     int (*feature_provider) (gameobject_t*);
-    double base_probability;   // Probability to place this feature
+    double max_probability;    // The modified probability shall never exceed this
+    double min_probability;    // The modified probability shall never subceed this
+    double base_probability;   // Probability to place this feature on one tile
     double minimum_distance;   // minimum distance between two of these features
-    double probability_growth; // this will be added to the base probability for every tile distance higher then minimum_distance
+    double probability_mod;    // this will be added to the base probability for every tile distance higher then minimum_distance
+    double probablity_growth;  // the probability will be multiplied by this for each tile of distance
+    double foreign_distance;   // minimum distance of this feature to other features
 } feature_t;
 
 /**
@@ -25,7 +30,7 @@ typedef struct Feature {
  * @param sector_cols How many sectors should there be horizontally
  * @param features What features should be generated (everything is a game object)
  * @param destination world_t struct to place everything in
- * @return int error codes ()
+ * @return int error codes (EINVAL, EDESTADDRREQ)
  */
 int generate_world(size_t sector_rows, size_t sector_cols, feature_t* features, world_t* destination);
 
