83fdc0a4f77e5f9bf8edbeb4ba7ab5877ed1b8de
Hyalunar <vegotenksGogeto@gmx.de>
2023-07-21 11:57:50 +0200

    Better make scrip

--- make.sh
+++ make.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
-gcc `find . -type f -path *.c` -o operational-space
+source_files=`find . -type f -path *.c`
+library_files=`find . -type f -path *.a`
+include_dirs=`find . -type d -path */include`
+include_args=`for include in $include_dirs; do echo "-I$include "; done` 
+CFLAGS="-Wall -Wextra -ggdb"
+gcc $source_files $library_files $include_args $CFLAGS -o operational-space
