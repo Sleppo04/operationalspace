4f0a3e7bc9eaa5057e9647c99651f22cac2213df
Hyalunar <vegotenksGogeto@gmx.de>
2023-07-21 14:04:52 +0200

    first error check in function

--- src/game/world.c
+++ src/game/world.c
@@ -1,6 +1,8 @@
 #include "world.h"
 
-int generate_world(size_t sector_rows, size_t sector_cols, world_t destination)
+int generate_world(size_t sector_rows, size_t sector_cols, feature_t* features, world_t* destination)
 {
-    
+    if (sector_rows == 0) {
+        return 
+    }
 }
