bd02e8847b0d03052c2a5766b384e6b20b7c834c
Hyalunar <vegotenksGogeto@gmx.de>
2023-07-22 18:01:45 +0200

    World generation test, one gcc warning, zero valgrind

--- tests/worldgen.c
+++ tests/worldgen.c
@@ -16,23 +16,32 @@ int feature_provider(gameobject_t** destination, void* user_data)
 int main()
 {
 	world_t world;
-	feature_t features[2];
 	unsigned short seed[3];
+	feature_t features[2];
 	seed[0] = 1;
 	seed[1] = 2;
 	seed[2] = 3;
 	features[0].provider = feature_provider;
-	features[0].base_probability = 0.1;
-	features[0].foreign_distance = 5;
-	features[0].minimum_distance = 5;
-	features[0].min_probability  = 0.001;
-	features[0].max_probability  = 0.75;
-	features[0].probability_mod  = 0.05;
-	features[0].probability_mod  = 1;
-	features[1].provider         = NULL;
+	features[0].base_probability    = 0.1;
+	features[0].foreign_distance    = 5;
+	features[0].minimum_distance    = 5;
+	features[0].min_probability     = 0.001;
+	features[0].max_probability     = 0.75;
+	features[0].probability_mod     = 0.05;
+	features[0].probability_growth  = 1;
+	features[1].provider            = NULL;
 	int world_code = generate_world(2, 2, features, &world, seed);
 
 	printf("generate_world exited with %i\n",world_code);
+
+	for (size_t row = 0; row < world.sector_rows * SECTOR_SIZE; row++) {
+		for (size_t col = 0; col < world.sector_cols * SECTOR_SIZE; col++) {
+			tile_t* tile = World_GetTile(&world, col, row);
+			if (tile->object) free(tile->object);
+		}
+	}
+
+	free(world.sectors);
 
 	return 0;
 }
