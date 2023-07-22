4c73436a7fbee692c0a3bddca05b58cf41b944db
Hyalunar <vegotenksGogeto@gmx.de>
2023-07-22 12:30:30 +0200

    weapon module struct members from stats.md

--- src/game/module.h
+++ src/game/module.h
@@ -27,6 +27,15 @@ typedef struct ArmorModule {
 typedef struct WeaponModule {
     module_t base;
     // weapon module members
+    int32_t armor_damage;
+    int32_t shield_damage;
+    int32_t area_armor_damage;
+    int32_t area_shield_damage;
+    int32_t armor_damage_area;
+    int32_t shield_damage_area;
+    int32_t cooldown;
+    int32_t activation_energy;
+    int32_t recharge_energy;
 } weapon_module_t;
 
 typedef struct EngineModule {
