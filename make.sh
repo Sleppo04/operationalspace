98202bedabe893f9b9e1398a236f24f2d9ccc26b
Hyalunar <vegotenksGogeto@gmx.de>
2023-07-22 18:36:45 +0200

    new library flag

--- make.sh
+++ make.sh
@@ -4,4 +4,5 @@ library_files=`find . -type f -path *.a`
 include_dirs=`find . -type d -path */include`
 include_args=`for include in $include_dirs; do echo "-I$include "; done` 
 CFLAGS="-Wall -Wextra -ggdb"
-gcc $source_files $library_files $include_args $CFLAGS -o operational-space
+LIBRARIES="-lm"
+gcc $source_files $library_files $include_args $CFLAGS $LIBRARIES -o operational-space
