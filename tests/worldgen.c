eeb53cb1be17dcfc5e04c7684e2ad6bc6c631b51
Hyalunar <vegotenksGogeto@gmx.de>
2023-07-22 18:29:42 +0200

    changed seed format

--- tests/worldgen.c
+++ tests/worldgen.c
@@ -4,7 +4,7 @@
 
 #include <stdio.h>
 
-int feature_provider(gameobject_t** destination, void* user_data)
+int feature_provider(gameobject_t** destination, void* user_data __attribute__((unused)))
 {
 	destination[0] = malloc(sizeof(gameobject_t));
 	destination[0]->color = 100;
@@ -16,18 +16,15 @@ int feature_provider(gameobject_t** destination, void* user_data)
 int main()
 {
 	world_t world;
-	unsigned short seed[3];
+	unsigned int seed = 15;
 	feature_t features[2];
-	seed[0] = 1;
-	seed[1] = 2;
-	seed[2] = 3;
 	features[0].provider = feature_provider;
 	features[0].base_probability    = 0.1;
 	features[0].foreign_distance    = 5;
 	features[0].minimum_distance    = 5;
 	features[0].min_probability     = 0.001;
-	features[0].max_probability     = 0.75;
-	features[0].probability_mod     = 0.05;
+	features[0].max_probability     = 0.25;
+	features[0].probability_mod     = 0.005;
 	features[0].probability_growth  = 1;
 	features[1].provider            = NULL;
 	int world_code = generate_world(2, 2, features, &world, seed);
