28fac5ca7c2cd6badbd5b222c2e13db8ef4cc0e6
Hyalunar <vegotenksGogeto@gmx.de>
2023-07-31 17:44:30 +0200

    Added battery to apply_modifier

--- src/game/stats.c
+++ src/game/stats.c
@@ -4,6 +4,7 @@ void Stats_ApplyModifier(stats_t* stats, stats_t* mods)
 {
     stats->battery += mods->battery;
     stats->storage += mods->storage;
+    stats->battery += mods->battery;
     stats->shield  += mods->shield;
     stats->speed   += mods->speed;
     stats->hull    += mods->hull;
