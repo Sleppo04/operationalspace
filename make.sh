bc92e74139a2c90775d344fef4e7339a11b3ae4b
Hyalunar <vegotenksGogeto@gmx.de>
2023-07-31 16:52:33 +0200

    Modified make.sh to write to bin/ Also added wren submodule in src/3rdparty

--- make.sh
+++ make.sh
@@ -5,7 +5,7 @@ include_dirs=`find . -type d -path */include`
 include_args=`for include in $include_dirs; do echo "-I$include "; done` 
 CFLAGS="-Wall -Wextra -ggdb"
 LIBRARIES="-lm"
-gcc $source_files $library_files $include_args $CFLAGS $LIBRARIES -o operational-space
+gcc $source_files $library_files $include_args $CFLAGS $LIBRARIES -o bin/operational-space
 
 # Compile terminal test utility
-gcc -x c $CFLAGS tests/ansitest.c.test src/ui/window.c -o os-termtest
+gcc -x c $CFLAGS tests/ansitest.c.test src/ui/window.c -o bin/os-termtest
