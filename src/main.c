aa05b88e280ef2a99decc9dc8fed40931bb63c5c
Hyalunar <vegotenksGogeto@gmx.de>
2023-07-31 16:49:41 +0200

    Purged squirrel out of this project

--- src/main.c
+++ src/main.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 
-//#include "squirrel.h"
 #include "ui/window.h"
 #include "ui/ui.h"
 #include "game/sector.h"
@@ -18,22 +17,22 @@ int main(int argc, char** argv)
 {
     window_t win;
     world_t world;
-	feature_t features[2];
+    feature_t features[2];
 	
     features[0].provider = feature_provider;
-	features[0].base_probability    = 0.001;
-	features[0].foreign_distance    = 5;
-	features[0].minimum_distance    = 5;
-	features[0].min_probability     = 0.001;
-	features[0].max_probability     = 0.01;
-	features[0].probability_mod     = 0.0;
-	features[0].probability_growth  = 1;
-	features[1].provider            = NULL;
+    features[0].base_probability    = 0.001;
+    features[0].foreign_distance    = 5;
+    features[0].minimum_distance    = 5;
+    features[0].min_probability     = 0.001;
+    features[0].max_probability     = 0.01;
+    features[0].probability_mod     = 0.0;
+    features[0].probability_growth  = 1;
+    features[1].provider            = NULL;
 
     generate_world(8, 8, features, &world, 42);
 
     Window_CreateWindow(80, 24, &win);
-	UI_Init(&world);
+    UI_Init(&world);
     Window_ClearScreen();
     UI_DrawFrame();
 	
