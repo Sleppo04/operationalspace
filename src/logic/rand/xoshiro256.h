b0440f4d87e809ba679927f5ae3ade4fb0a367b2
Hyalunar <vegotenksGogeto@gmx.de>
2023-07-31 18:16:00 +0200

    took xoshiro256 random generator from https://prng.di.unimi.it/xoshiro256plusplus.c (CC0) and modified it to make it usable

---
+++ src/logic/rand/xoshiro256.h
@@ -0,0 +1,12 @@
+#ifndef XOSHIRO256_H
+#define XOSHIRO256_H
+
+#include <stdint.h>
+
+typedef struct XoshiroState {
+    uint64_t state[4];
+} xoshiro256_state_t;
+
+uint64_t rotate_uint64_left(uint64_t bytes, int degree);
+
+#endif
