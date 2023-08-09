ca61b0803ed0925705fdac42fe9f37482adc9d61
Hyalunar <vegotenksGogeto@gmx.de>
2023-08-09 15:05:07 +0200

    Moved definitions, added function declarations

--- src/game/game.h
+++ src/game/game.h
@@ -1,21 +1,10 @@
 #ifndef GAME_H
 #define GAME_H
 
-#include "gameobject.h"
-#include "player.h"
-#include "world.h"
-#include "tick.h"
-
-typedef struct Game {
-	tick_type_t tick_type;
-	tick_data_t tick_information;
-	player_t*   players;
-	uint16_t    player_count;
-	world_t*    world;
-} game_t;
+#include "shared.h"
 
 int Game_TickRound(game_t* game);
-int Game_TickStep(game_t* game);   // For future implementation
-int Game_TickSingle(game_t* game); // For future implementation
+int Game_TickStep(game_t* game); 
+int Game_TickSingle(game_t* game);
 
 #endif
--- src/game/tick.h
+++ src/game/tick.h
@@ -1,18 +1,14 @@
 #ifndef TICK_H
 #define TICK_H
 
-typedef enum TickType {
-	TICK_PLAYERWISE
-} tick_type_t;
+#include "shared.h"
 
-typedef struct TickPlayerWiseData {
-	uint16_t current_player_index;
-} tick_playerwise_data_t;
 
-typedef union TickData {
-	tick_playerwise_data_t playerwise;
-} tick_data_t;
+//TODO: Argument to allow access to the world
+int Tick_Playerwise_Round(tick_playerwise_data_t* data, game_t* game);
 
-int Tick_PerformByType(enum TickType type, union TickData data, game_t* game);
+int Tick_Playerwise_Step(tick_playerwise_data_t* data, game_t* game);
+
+int Tick_Playerwise_Single(tick_playerwise_data_t* data, game_t* game);
 
 #endif //TICK_H
