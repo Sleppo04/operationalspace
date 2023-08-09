a1855d1a4c5cdf2bf350fb5e91fd25619dd2f41d
Hyalunar <vegotenksGogeto@gmx.de>
2023-08-09 16:21:00 +0200

    Added the ability to activate a module in a certain direction

--- src/game/action.h
+++ src/game/action.h
@@ -1,11 +1,14 @@
 #ifndef GAMEACTION_H
 #define GAMEACTION_H
 
+#include <stdint.h>
+
 #include "shared.h"
 
 typedef enum ActionType {
     ACTION_MOVEMENT,
-    ACTION_MODULE
+    ACTION_MODULE,
+    ACTION_DIRECTION_MODULE
 } action_type_t;
 
 typedef struct MovementActionData {
@@ -16,6 +19,13 @@ typedef struct ModuleActionData {
     bool activation_state;
     uint32_t module_id;
 } module_action_data_t;
+
+typedef struct DirectionModuleActionData {
+    bool activation_state;
+    uint32_t module_id;
+    int32_t  x_direction;
+    int32_t  y_direction;
+} direction_module_action_data_t;
 
 typedef union ActionData {
     movement_action_data_t movement;
