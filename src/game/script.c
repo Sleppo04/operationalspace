b55f333658bac62e24d9c5d986465bc0ab8f9ce3
Hyalunar <vegotenksGogeto@gmx.de>
2023-08-09 17:18:38 +0200

    threading header, script function minimal body

---
+++ src/game/script.c
@@ -0,0 +1,13 @@
+#include "script.h"
+
+int Script_ComputeShipActions(script_t* script, game_t* game)
+{
+    if (script == NULL) {
+        return EINVAL;
+    }
+    if (game == NULL) {
+        return EINVAL;
+    }
+
+    return 0;
+}
