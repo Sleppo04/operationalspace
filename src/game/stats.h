7c2b2d1d02e8f7aa358dc6993b8c1dfb78a72846
Hyalunar <vegotenksGogeto@gmx.de>
2023-08-14 14:49:10 +0200

    Alternative stat layout suggestion

--- src/game/stats.h
+++ src/game/stats.h
@@ -2,6 +2,26 @@
 #define STATS_H
 
 #include <stdint.h>
+#include <stdbool.h>
+
+typedef struct ExtendedStats {
+    int32_t hull;
+    int32_t shield;
+    int32_t speed;
+    int32_t mass;
+    int32_t storage;
+    int32_t battery;
+    int32_t energy_consumption;
+    int32_t energy_regeneration;
+    int32_t shield_regeneration;
+    int32_t hull_regeneration;
+} extended_stats_t;
+
+typedef struct ModuleStats {
+    extended_stats_t base_stats;
+    bool activatable;
+    extended_stats_t activatable_stats;
+} module_stats_t;
 
 typedef struct stats_s
 {
