cbc347a85a8d35a255371dc093b737d873a48a1b
Hyalunar <vegotenksGogeto@gmx.de>
2023-08-15 22:33:27 +0200

    config

---
+++ src/game/config.h
@@ -0,0 +1,28 @@
+#ifndef GAMECONFIG_H
+#define GAMECONFIG_H
+
+#include <stdint.h>
+#include <stdlib.h>
+
+typedef struct ScriptConfig {
+    size_t   MaxScriptMemory;
+    uint64_t MaxScriptOpcodes;
+    time_t   ScriptTimeoutSeconds;
+} script_config_t;
+
+typedef struct WorldConfig {
+    uint16_t sector_rows;
+    uint16_t sector_cols;
+} world_config_t;
+
+typedef struct ModuleConfig {
+    char* source_root_directory;
+} module_config_t;
+
+typedef struct GameConfig {
+    script_config_t script;
+    world_config_t  world;
+    module_config_t module;
+} config_t;
+
+#endif
