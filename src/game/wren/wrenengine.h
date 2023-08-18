5506963629c6ec6ef878d9e302f4926b13188da8
Hyalunar <vegotenksGogeto@gmx.de>
2023-08-18 15:07:01 +0200

    Logging for the WrenEngine

--- src/game/wren/wrenengine.h
+++ src/game/wren/wrenengine.h
@@ -3,10 +3,18 @@
 
 #include "wren.h"
 #include "wrenerror.h"
+#include "wrenlogs.h"
 
 typedef struct WrenEngine {
     WrenVM*    vm;
+
+    // Array of error reports
+    // if the first element is of type COMPILE_ERROR, then the array is of length 1
+    // if the first element is of type RUNTIME_ERROR, the following elements will be stack-traces
     WrenError* errors;
+
+    // Script output (via print())
+    WrenLogs*  logs;
 } wren_engine_t;
 
 #endif //WRENENGINE_H
