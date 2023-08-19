6bd9eb155e632676517d9023d2357c0465120776
Hyalunar <vegotenksGogeto@gmx.de>
2023-08-19 14:51:00 +0200

    Removed secondary wren references

--- src/game/script.h
+++ src/game/script.h
@@ -3,15 +3,11 @@
 
 #include "world.h"
 
-#include "wren.h"
-#include "wren/wrenengine.h"
-
 struct Game;
 
 typedef int (*CScriptFunction) (struct Game* game, void** data_pointer);
 
 typedef enum ScriptType {
-	SCRIPT_WREN,
 	SCRIPT_C_FUNC
 } script_type_t;
 
@@ -19,13 +15,8 @@ typedef struct CSCriptData {
 	CScriptFunction c_function;
 } c_script_data_t;
 
-typedef struct WrenScriptData {
-	WrenHandle* function_handle;
-} wren_script_data_t;
-
 typedef union ScriptData {
 	struct CSCriptData c_data;
-	struct WrenScriptData wren_data;
 } script_data_t;
 
 typedef struct Script {
@@ -36,7 +27,6 @@ typedef struct Script {
 typedef struct PlayerScripts {
 	script_t      ship_tick_script;
 	script_t      round_tick_script;
-	wren_engine_t wren;
 	void*         c_data;
 } player_scripts_t;
 
