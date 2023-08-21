7e7a903088a8a8d11331adb2b21bedcddf6fd93e
Hyalunar <vegotenksGogeto@gmx.de>
2023-08-21 22:32:09 +0200

    Disabled bugged warning

--- make.sh
+++ make.sh
@@ -11,8 +11,9 @@ library_files=`find . -type f -path *.a`
 include_dirs=`find . -type d -path */include`
 include_args=`for include in $include_dirs; do echo "-I$include "; done` 
 CFLAGS="-std=c99 -pedantic -Wall -Wextra -ggdb"
+DISABLED_BUG_WARNINGS="-Wno-missing-braces"
 LIBRARIES="-lm -pthread"
-gcc $source_files $library_files $include_args $CFLAGS $LIBRARIES -o bin/operational-space
+gcc $source_files $library_files $include_args $CFLAGS $LIBRARIES $DISABLED_BUG_WARNINGS -o bin/operational-space
 
 # Compile terminal test utility
 gcc -x c $CFLAGS tests/ansitest.c.test src/ui/window.c -o bin/os-termtest
