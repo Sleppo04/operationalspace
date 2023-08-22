dd56c76645a02e79553de68dc3bd13f04d2f4697
Hyalunar <vegotenksGogeto@gmx.de>
2023-08-22 23:02:44 +0200

    CustomType type

--- src/logic/ubc/types.h
+++ src/logic/ubc/types.h
@@ -17,4 +17,21 @@ typedef struct UbcString
     unsigned int length;
 } ubcString_t;
 
+enum UbcType {
+    UBCTYPE_INTEGER,
+    UBCTYPE_BOOLEAN,
+    UBCTYPE_CUSTOM,
+    UBCTYPE_STRING,
+    //UBCTYPE_VECTOR
+    UBCTYPE_FLOAT,
+};
+
+typedef struct UbcCustomType
+{
+    enum UbcType field_types;
+    char*   field_names;
+    void*   fields;
+    int     field_count;
+} ubcCustomType_t;
+
 #endif //UBCTYPES
