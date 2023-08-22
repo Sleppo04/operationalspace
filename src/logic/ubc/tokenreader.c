34c95f0f5354c9354188f62eed69f8a4eeab73f2
Sleppo04 <gamedisk2000@gmail.com>
2023-08-22 16:13:38 +0200

    Cleaned up UBC Tokenreader

--- src/logic/ubc/tokenreader.c
+++ src/logic/ubc/tokenreader.c
@@ -1,23 +1,25 @@
 #include "tokenreader.h"
 
+#define MAPTOKEN(x,y) if (strncmp(x, token->ptr, token->value.length) == 0) token->type = y
+
 void Ubc_ReadNextToken(lexer_t* lexer, token_t* token)
 {
     Lexer_NextToken(lexer, token);
 
     if (token->type == TT_IDENTIFIER) {
-        if (strncmp("while", token->ptr, token->value.length) == 0)    token->type = TT_UBC_WHILE;
-        if (strncmp("if", token->ptr, token->value.length) == 0)       token->type = TT_UBC_IF;
-        if (strncmp("elif", token->ptr, token->value.length) == 0)     token->type = TT_UBC_ELIF;
-        if (strncmp("else", token->ptr, token->value.length) == 0)     token->type = TT_UBC_ELSE;
-        if (strncmp("break", token->ptr, token->value.length) == 0)    token->type = TT_UBC_BREAK;
-        if (strncmp("return", token->ptr, token->value.length) == 0)   token->type = TT_UBC_RETURN;
-        if (strncmp("function", token->ptr, token->value.length) == 0) token->type = TT_UBC_FUNCTION;
-        if (strncmp("var", token->ptr, token->value.length) == 0)      token->type = TT_UBC_VAR;
-        if (strncmp("type", token->ptr, token->value.length) == 0)     token->type = TT_UBC_TYPE;
-        if (strncmp("persist", token->ptr, token->value.length) == 0)  token->type = TT_UBC_PERSIST;
-        if (strncmp("include", token->ptr, token->value.length) == 0)  token->type = TT_UBC_INCLUDE;
-        if (strncmp("false", token->ptr, token->value.length) == 0)    token->type = TT_UBC_FALSE;
-        if (strncmp("true", token->ptr, token->value.length) == 0)     token->type = TT_UBC_TRUE;
+        MAPTOKEN("while", TT_UBC_WHILE);
+        MAPTOKEN("if", TT_UBC_IF);
+        MAPTOKEN("elif", TT_UBC_ELIF);
+        MAPTOKEN("else", TT_UBC_ELSE);
+        MAPTOKEN("break", TT_UBC_BREAK);
+        MAPTOKEN("return", TT_UBC_RETURN);
+        MAPTOKEN("function", TT_UBC_FUNCTION);
+        MAPTOKEN("var", TT_UBC_VAR);
+        MAPTOKEN("type", TT_UBC_TYPE);
+        MAPTOKEN("persist", TT_UBC_PERSIST);
+        MAPTOKEN("include", TT_UBC_INCLUDE);
+        MAPTOKEN("false", TT_UBC_FALSE);
+        MAPTOKEN("true", TT_UBC_TRUE);
     }
 
     return;
