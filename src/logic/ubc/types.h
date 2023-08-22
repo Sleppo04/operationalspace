f0083b7960bce34c0410b0409d7424e330ef32a2
Sleppo04 <gamedisk2000@gmail.com>
2023-08-22 09:27:36 +0200

    Added String type to UBC

--- src/logic/ubc/types.h
+++ src/logic/ubc/types.h
@@ -3,11 +3,18 @@
 
 #include <stdint.h>
 
-typedef struct UbcFunction {
+typedef struct UbcFunction
+{
     uint16_t constant_count;
     uint16_t variable_count;
     char* bytecode;
     uintptr_t bytecode_length;
 } ubcfunction_t;
+
+typedef struct UbcString
+{
+    unsigned int memOffset;
+    unsigned int length;
+} ubcString_t;
 
 #endif //UBCTYPES
