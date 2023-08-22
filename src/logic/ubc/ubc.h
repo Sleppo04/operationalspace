ca171379f6babde6ef082b892275813e0333369c
Sleppo04 <gamedisk2000@gmail.com>
2023-08-22 19:38:37 +0200

    Removed STACK_OVERFLOW flag from UBC

--- src/logic/ubc/ubc.h
+++ src/logic/ubc/ubc.h
@@ -6,12 +6,12 @@
 // Status register flags (bit masks)
 #define UBC_ST_ZERO 0x01
 #define UBC_ST_OVFL 0x02 // Arithmetic overflow
-#define UBC_ST_STOF 0x04 // https://stackoverflow.com
-#define UBC_ST_RES1 0x08 // Reserved for future use
-#define UBC_ST_RES2 0x10 // ...
-#define UBC_ST_RES3 0x20 // ...
-#define UBC_ST_RES4 0x40 // ...
-#define UBC_ST_RES5 0x80 // ...
+#define UBC_ST_RES1 0x04 // Reserved for future use
+#define UBC_ST_RES2 0x08 // ...
+#define UBC_ST_RES3 0x10 // ...
+#define UBC_ST_RES4 0x20 // ...
+#define UBC_ST_RES5 0x40 // ...
+#define UBC_ST_RES6 0x80 // ...
 
 // Opcodes
 #define UBC_OP_HLT     0x00
@@ -21,8 +21,8 @@
 #define UBC_OP_JMPi    0x03
 #define UBC_OP_JZ      0x04
 #define UBC_OP_JOV     0x05
-#define UBC_OP_JSO     0x06
-#define UBC_OP_RES1    0x07
+#define UBC_OP_RES1    0x06
+#define UBC_OP_RES2    0x07
 // Math functions
 #define UBC_OP_ADDI    0x08
 #define UBC_OP_ADDF    0x09
