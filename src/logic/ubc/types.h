87cf357f5d8ac5049b0a235b6df9642b8fcd4a1e
Hyalunar <vegotenksGogeto@gmx.de>
2023-08-23 16:12:23 +0200

    Removed due to architecture changes unused type

--- src/logic/ubc/types.h
+++ src/logic/ubc/types.h
@@ -3,14 +3,6 @@
 
 #include <stdint.h>
 
-typedef struct UbcFunction
-{
-    uint16_t constant_count;
-    uint16_t variable_count;
-    char* bytecode;
-    uintptr_t bytecode_length;
-} ubcfunction_t;
-
 typedef struct UbcString
 {
     unsigned int memOffset;
