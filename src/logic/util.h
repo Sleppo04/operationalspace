68f5619b5100499d8cd1390b1163316dcc394077
Hyalunar <vegotenksGogeto@gmx.de>
2023-09-13 19:56:24 +0200

    strnchr for safer strchr

--- src/logic/util.h
+++ src/logic/util.h
@@ -10,12 +10,27 @@ char* Sys_Strdup(char* s)
     size_t len;
 
     len = strlen(s);
-    if (len == NULL) return NULL;
+    if (len == 0) return NULL;
     dup = (char*) malloc(len + 1);
     if (dup == NULL) return NULL;
     memcpy(dup, s, len + 1);
     
     return dup;
+}
+
+char* strnchr(char* string, char character, size_t limit)
+{
+    for (size_t i = 0; i < limit; i++) {
+        if (string[i] == character) {
+            return string + i;
+        }
+
+        if (string[i] == '\0') {
+            i = limit;
+        }
+    }
+
+    return NULL;
 }
 
 #endif
