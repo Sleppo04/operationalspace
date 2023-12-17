410192f2d6b9ae887e69cd7962c06ec895406422
Sleppo04 <gamedisk2000@gmail.com>
2023-12-17 19:16:28 +0100

    First steps

---
+++ src/server/s_world.h
@@ -0,0 +1,40 @@
+#ifndef S_WORLD_H
+#define S_WORLD_H
+
+#include <errno.h>
+#include <float.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#include <world.h>
+#include <sector.h>
+#include <feature.h>
+
+#include "../logic/arraylist/arraylist.h"
+#include "../logic/coordinate/coordinate.h"
+#include "../logic/rand/xoshiro256.h"
+
+
+void World_Create(world_t* world, unsigned int width, unsigned int height);
+
+/// @brief get a sector at row and col from the world
+/// @param world world to get the sector from
+/// @param row in which row is the sector
+/// @param col what is the column of the sector
+/// @param destination where the result pointer will be written
+/// @return int, errorcodes (EINVAL, EDESTADDRREQ)
+int World_GetSector(world_t* world, size_t row, size_t col, sector_t** destination);
+
+/// @brief Get a tile from its absolute address 
+/// @param x The tiles absolute x-position
+/// @param y The tiles absolute y-position
+/// @return Pointer to the requested tile struct
+tile_t* World_GetTile(world_t* world, int x, int y);
+
+/// @brief Dump world in a raw binary format for debugging
+/// @param filename The name of the file to dump the world to
+void World_DebugDump(world_t* world, char* filename);
+
+#endif // S_WORLD_H
